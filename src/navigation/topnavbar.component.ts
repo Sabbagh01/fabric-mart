0a09937ad10096af7bfe2f45f63b6cbd2d605b0a
Sabbagh01 <ma00017@mix.wvu.edu>
2022-01-20 15:58:09 -0500

    aa

---
+++ src/navigation/topnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'fm-topnavbat',
+    templateUrl: 'topnavbar.component.html',
+    styleUrls: ['topnavbar.component.css']
+})
+export class TopNavBarComponent{
+
+}
